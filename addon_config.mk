02d1eac74b433d82a3def13d526d19b10e01af30
Andrew McWilliams <andy@jahya.net>
2014-10-03 22:24:43 -0400

    Adds make files

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxKinectV2OSC
+	ADDON_DESCRIPTION = Get Kinect V2 sensor data into openFrameworks over OSC
+	ADDON_AUTHOR = Andrew McWilliams
+	ADDON_TAGS = osc skeleton full-body interaction kinect kinect2 kinectv2
+	ADDON_URL = http://github.com/microcosm/ofxKinectV2OSC
+
+common:
+	ADDON_DEPENDENCIES = ofxOsc
